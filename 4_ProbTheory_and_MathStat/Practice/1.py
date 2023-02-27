fe2d23edc3021a67877ecb43d86d16d48706587e
behappyman <behappyman@mail.ru>
2023-02-27 20:19:33 +0300

    PT&MS

---
+++ 4_ProbTheory_and_MathStat/Practice/1.py
@@ -0,0 +1,53 @@
+import numpy as np
+from Practice.utils import my_print, my_combinations, my_arrangements
+
+# -------------------------------------------------------------------------------------------
+test_array = np.random.randint(0, 10, 1000000)
+
+my_print(msg=f"Массив: {test_array}",
+         color='green', separator_sym='*', separator_before=True, separator_after=False)
+my_print(msg=f"Относительная частота события 'генерация числа 3': {len(test_array[test_array==3]) / len(test_array)}",
+         color='blue', separator_sym='*', separator_before=False, separator_after=True)
+
+# -------------------------------------------------------------------------------------------
+my_print(msg='Подбрасываются две игральные кости, подсчитать частоту события, при котором на первой игральной кости '
+             'выпала 1, а на второй 6', separator_before=False, separator_after=False)
+
+cube_1 = np.random.randint(1, 7, 10000)
+cube_2 = np.random.randint(1, 7, 10000)
+event = cube_1[(cube_1 == 1) & (cube_2 == 6)]
+my_print(msg=f'Частота интересующего события: {len(event) / len(cube_1)}', color='blue',
+         separator_after=True, separator_before=False)
+
+# -------------------------------------------------------------------------------------------
+my_print(msg='сколькими способами можно выбрать 4 карты из колоды в 36 карт',
+         separator_before=False, separator_after=False)
+
+my_print(msg=f'{my_combinations(36, 4)}', color='blue', separator_before=False, separator_after=True)
+
+# -------------------------------------------------------------------------------------------
+my_print(msg='сколькими способами можно выстроить очередь из 5 человек при общем количестве людей - 20',
+         separator_before=False, separator_after=False)
+
+my_print(msg=f'{my_arrangements(20, 5)}', color='blue', separator_before=False, separator_after=True)
+
+# ------------------------------------------------------------------------------------------
+my_print(msg='сколькими способами  5 человек могут организовать очередь',
+         separator_before=False, separator_after=False)
+
+my_print(msg=f'{my_arrangements(5, 5)}', color='blue', separator_before=False, separator_after=True)
+
+# ------------------------------------------------------------------------------------------
+my_print(msg='из колоды состоящей из 36 карт случайным образом выбрано 5. Сколькими способами можно выбрать эти карты '
+             'так, чтобы среди них оказалось от 2 до 3 тузов',
+         separator_before=False, separator_after=False)
+
+my_print(msg=str(my_combinations(4, 2) * my_combinations(32, 3) + my_combinations(4, 3) * my_combinations(32, 2)),
+         separator_before=False, color='blue', separator_after=True)
+
+# ------------------------------------------------------------------------------------------
+my_print(msg='Какова вероятность выбрать из колоды в 36 карт 5 карт так, чтобы среди них было 2 туза',
+         separator_before=False, separator_after=False)
+
+my_print(msg=f"{round(my_combinations(4, 2) * my_combinations(32, 3) / my_combinations(36, 5) * 100, 2)}%",
+         separator_before=False, color='blue', separator_after=True)
---
+++ 4_ProbTheory_and_MathStat/Practice/utils.py
@@ -0,0 +1,38 @@
+from colorama import Fore, Style
+
+
+def my_print(msg: str, color='green', separator_sym="*", separator_before=False, separator_after=False) -> None:
+    colors = {'yellow': Fore.YELLOW, 'green': Fore.GREEN, 'red': Fore.RED,
+              'blue': Fore.BLUE, 'magenta': Fore.MAGENTA, '1': Fore.WHITE}
+    if separator_before:
+        print(f"{Fore.RED}{separator_sym * 300}{Style.RESET_ALL}")
+    if color in colors:
+        print(f"{colors[color]}{msg}{Style.RESET_ALL}")
+    else:
+        print(f"{msg}")
+    if separator_after:
+        print(f"{Fore.RED}{separator_sym * 300}{Style.RESET_ALL}")
+
+# ------------------------------------------------math--------------------------------------------------
+
+
+def my_factorial(n: int) -> int:
+    '''подсчет факториала числа n'''
+    if n == 0:
+        return 1
+    else:
+        return n * my_factorial(n-1)
+
+
+def my_combinations(n: int, k: int) -> int:
+    '''подсчет числа сочетаний из n элементов по k'''
+    return int(my_factorial(n) / (my_factorial(k) * my_factorial(n - k)))
+
+def my_arrangements(n: int, k: int) -> int:
+    '''подсчет числа размещений из n элементов по k'''
+    return int(my_factorial(n) / (my_factorial(n - k)))
+
+def my_permutations(n: int) -> int:
+    '''подсчет числа перестановок из n элементов'''
+    return my_arrangements(n, n)
+
